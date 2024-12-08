59d879fea9b09ff1586f79c929d6ee8e1be16b2e
divitkalathil <dakalath@ncsu.edu>
2024-12-08 18:50:06 -0500

    Defined Category Controller and Services

--- productcatalog/src/main/java/com/ecom/productcatalog/model/Category.java
+++ productcatalog/src/main/java/com/ecom/productcatalog/model/Category.java
@@ -1,18 +1,19 @@
 package com.ecom.productcatalog.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import java.util.Set;
 
+@Entity
+@Data
+@Table(name = "category") // Optional but useful to specify the table name
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+
     private String name;
 
-    @OneToMany(mappedBy = "category",
-    cascade = CascadeType.ALL,
-    fetch =FetchType.LAZY)
-
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Product> products;
 }
--- productcatalog/src/main/java/com/ecom/productcatalog/model/Product.java
+++ productcatalog/src/main/java/com/ecom/productcatalog/model/Product.java
@@ -9,13 +9,13 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String description;
     private String imageUrl;
     private Double price;
 
     @ManyToOne
-    @JoinColumn(name="category_id", nullable=false)
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
-
 }
